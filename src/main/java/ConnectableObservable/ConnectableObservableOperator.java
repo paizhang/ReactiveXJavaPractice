3bb1e21d0cf9462cd97e6792ab9fb3a38989c18f
paizhang <zhangpai91@gmail.com>
2019-12-05 13:03:51 -0800

    Add example for using refCount for converting ConnectableObservable into Cold Observable.

--- src/main/java/ConnectableObservable/ConnectableObservableOperator.java
+++ src/main/java/ConnectableObservable/ConnectableObservableOperator.java
@@ -7,6 +7,9 @@ import io.reactivex.schedulers.Schedulers;
 
 import java.util.concurrent.TimeUnit;
 
+/*
+    A nice article explaining ConnectableObservable: https://www.jianshu.com/p/575ce5b98389
+ */
 public class ConnectableObservableOperator {
 
     /*
@@ -29,7 +32,8 @@ public class ConnectableObservableOperator {
         By default, all subscribers all run in a computation thread. We can assign each subscriber a distinct thread by specifying a scheduler in
         the parameters of the operator.
 
-        TODO: It seems the reconnect part is not implemented in Rxjava. You can follow up with this feature. 
+        TODO: It seems the reconnect part is not implemented in Rxjava. You can follow up with this feature.
+
      */
     public void testUsingReplyAndSubscribeAndConnect() throws InterruptedException {
         ConnectableObservable<Long> connectableObservable = Observable.interval(1, TimeUnit.SECONDS).replay(2, Schedulers.computation());
@@ -44,4 +48,46 @@ public class ConnectableObservableOperator {
         disposable.dispose();
         Thread.sleep(10000);
     }
+
+    public void testUsingRefCount() throws InterruptedException {
+        /*
+            This is an example showing how subscription works when two observers subscribe the same ordinary observable at different time. Basically
+            two observers will receive different streams running in different threads started from the time they subscribe.
+         */
+        /*
+        Observable<Long> observable = Observable.interval(1, TimeUnit.SECONDS);
+        observable.subscribe(num -> System.out.println("Thread: " + Thread.currentThread().getName() + " First observer:" + num));
+        Thread.sleep(2000);
+        observable.subscribe(num -> System.out.println("Thread: " + Thread.currentThread().getName() + " Second observer:" + num));
+        Thread.sleep(10000);
+        */
+
+        /*
+            This is an example for converting a ConnectableObservable created using replay() into an Observable. The converted Observable is a cold
+            observable. That means it will start emit items when the first observer starts to subscribe. But this converted observable is not a ordinary
+            Observable that we see previously. The difference is that this converted observable still keeps the "replay property". When the second
+            observer subscribes, it receives all items in the buffer along with the following items from the time it subscribes.
+         */
+        /*
+        Observable<Long> observable = Observable.interval(1, TimeUnit.SECONDS).replay(2).refCount();
+        observable.subscribe(num -> System.out.println("Thread: " + Thread.currentThread().getName() + " First observer:" + num));
+        Thread.sleep(5000);
+        observable.subscribe(num -> System.out.println("Thread: " + Thread.currentThread().getName() + " Second observer:" + num));
+        Thread.sleep(10000);
+        */
+
+        /*
+            This is an example for converting a ConnectableObservable created using publish() into an Observable. The converted Observable is a cold
+            observable. That means it will start emit items when the first observer starts to subscribe. But this converted observable is not a ordinary
+            Observable that we see previously. The difference is that this converted observable still keeps the "publish property". When the second
+            observer subscribes, it receives items from the time it subscribes.
+         */
+        Observable<Long> observable = Observable.interval(1, TimeUnit.SECONDS).publish().refCount();
+        observable.subscribe(num -> System.out.println("Thread: " + Thread.currentThread().getName() + " First observer:" + num));
+        Thread.sleep(5000);
+        observable.subscribe(num -> System.out.println("Thread: " + Thread.currentThread().getName() + " Second observer:" + num));
+        Thread.sleep(10000);
+        
+    }
+
 }
